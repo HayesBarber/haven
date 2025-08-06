36c856de2d6ff930b0ba2683ce6e3b47834d1e42
Hayes Barber <barberhayes@gmail.com>
2025-08-05 20:24:59 -0400

    init splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:forui/forui.dart';
+import 'package:haven/screens/splash.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const Application());
+  runApp(const Haven());
 }
 
-class Application extends StatelessWidget {
-  const Application({super.key});
+class Haven extends StatelessWidget {
+  const Haven({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -20,34 +21,7 @@ class Application extends StatelessWidget {
       localizationsDelegates: const [...FLocalizations.localizationsDelegates],
       builder: (_, child) => FTheme(data: theme, child: child!),
       theme: theme.toApproximateMaterialTheme(),
-      home: const FScaffold(child: Example()),
+      home: Splash(),
     );
   }
-}
-
-class Example extends StatefulWidget {
-  const Example({super.key});
-
-  @override
-  State<Example> createState() => _ExampleState();
-}
-
-class _ExampleState extends State<Example> {
-  int _count = 0;
-
-  @override
-  Widget build(BuildContext context) => Center(
-    child: Column(
-      mainAxisSize: MainAxisSize.min,
-      spacing: 10,
-      children: [
-        Text('Count: $_count'),
-        FButton(
-          onPress: () => setState(() => _count++),
-          suffix: const Icon(FIcons.chevronsUp),
-          child: const Text('Increase'),
-        ),
-      ],
-    ),
-  );
 }
