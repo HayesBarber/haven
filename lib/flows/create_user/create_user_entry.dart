9aa00a83d413d828ad58dfec31ffb2e0ed3222cc
Hayes Barber <barberhayes@gmail.com>
2025-08-06 20:20:38 -0400

    auto commit

--- lib/flows/create_user/create_user_entry.dart
+++ lib/flows/create_user/create_user_entry.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:forui/forui.dart';
 import 'package:haven/flows/create_user/provider/create_user_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -9,7 +10,33 @@ class CreateUserEntry extends StatelessWidget {
     final provider = Provider.of<CreateUserProvider>(context);
 
     return Scaffold(
-      body: SizedBox.expand(child: Center(child: Text(provider.title))),
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Center(
+            child: Text(
+              "Create Username",
+              style: Theme.of(context).textTheme.displayMedium,
+            ),
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32),
+            child: FTextField(
+              label: const Text('Username'),
+              hint: "Jane Doe",
+              description: const Text('Please enter your username.'),
+              maxLines: 1,
+              onSubmit: (value) {},
+            ),
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 32),
+            child: FButton(onPress: () {}, child: const Text("Next")),
+          ),
+        ],
+      ),
     );
   }
 }
