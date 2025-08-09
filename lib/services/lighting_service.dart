9c8b9f3380ba4296d0bb4cfa24fd20e980cf839f
Hayes Barber <barberhayes@gmail.com>
2025-08-09 16:56:13 -0400

    create response extension

--- lib/services/lighting_service.dart
+++ lib/services/lighting_service.dart
@@ -1,5 +1,18 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:haven/utils/http_interceptors.dart';
+import 'package:home_api_client/home_api_client.dart';
+
 class LightingService {
   LightingService._();
   static final LightingService _instance = LightingService._();
   static LightingService get I => _instance;
+
+  Future<void> readDevices() async {
+    final api = HomeApiClient(
+      basePathOverride: dotenv.get('HOME_API_URL'),
+      interceptors: HttpInterceptors.getInterceptors(),
+    ).getDeviceApi();
+
+    final response = await api.readAllDevicesDeviceReadGet();
+  }
 }
