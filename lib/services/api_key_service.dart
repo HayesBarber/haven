7163da8837e5252fe983c8af6a4bdd196c1dc742
Hayes Barber <barberhayes@gmail.com>
2025-08-09 12:11:58 -0400

    auto commit

--- lib/services/api_key_service.dart
+++ lib/services/api_key_service.dart
@@ -1,7 +1,14 @@
 import 'package:gatekeeper_client/gatekeeper_client.dart';
+import 'package:haven/services/local_storage.dart';
 
 class ApiKeyService {
   ChallengeVerificationResponse? _apiKey;
+
+  Future<void> _initAsync() async {
+    final stored = await LocalStorage.I.read(StorageKey.apiKey);
+
+    if (stored == null) return;
+  }
 
   void _getApiKey() async {}
 }
