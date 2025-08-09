258de919a081b153e16867f0cdb18d07273e00d0
Hayes Barber <barberhayes@gmail.com>
2025-08-09 17:02:38 -0400

    return result

--- lib/services/lighting_service.dart
+++ lib/services/lighting_service.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:haven/utils/extensions.dart';
 import 'package:haven/utils/http_interceptors.dart';
+import 'package:haven/utils/result.dart';
 import 'package:home_api_client/home_api_client.dart';
 
 class LightingService {
@@ -7,12 +9,21 @@ class LightingService {
   static final LightingService _instance = LightingService._();
   static LightingService get I => _instance;
 
-  Future<void> readDevices() async {
-    final api = HomeApiClient(
-      basePathOverride: dotenv.get('HOME_API_URL'),
-      interceptors: HttpInterceptors.getInterceptors(),
-    ).getDeviceApi();
+  Future<Result<List<DeviceConfig>, Exception>> readDevices() async {
+    try {
+      final api = HomeApiClient(
+        basePathOverride: dotenv.get('HOME_API_URL'),
+        interceptors: HttpInterceptors.getInterceptors(),
+      ).getDeviceApi();
 
-    final response = await api.readAllDevicesDeviceReadGet();
+      final response = await api.readAllDevicesDeviceReadGet();
+      response.assertValid();
+
+      final data = response.data!;
+
+      return Success(data.devices.toList());
+    } catch (e) {
+      return Failure(Exception('Failed to read devices: $e'));
+    }
   }
 }
