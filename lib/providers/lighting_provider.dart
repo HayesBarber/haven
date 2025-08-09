0cc72a440d97fac6368ea68d3c8e021c2f1a41d5
Hayes Barber <barberhayes@gmail.com>
2025-08-09 18:57:38 -0400

    remove setters

--- lib/providers/lighting_provider.dart
+++ lib/providers/lighting_provider.dart
@@ -6,8 +6,8 @@ import 'package:home_api_client/home_api_client.dart';
 
 class LightingProvider extends ChangeNotifier {
   List<DeviceConfig> _deviceConfigs = [];
-  Map<Room, List<DeviceConfig>> _roomMap = {};
-  bool _isLoading = false;
+  Map<Room, List<DeviceConfig>> _roomsMap = {};
+  bool _loading = false;
   bool _hasError = false;
 
   LightingProvider() {
@@ -15,34 +15,18 @@ class LightingProvider extends ChangeNotifier {
   }
 
   List<DeviceConfig> get devices => _deviceConfigs;
-  set _devices(List<DeviceConfig> devices) {
-    _deviceConfigs = devices;
-  }
-
-  Map<Room, List<DeviceConfig>> get roomsMap => _roomMap;
-  set _roomsMap(Map<Room, List<DeviceConfig>> rooms) {
-    _roomMap = rooms;
-  }
-
-  bool get loading => _isLoading;
-  set _loading(bool loading) {
-    _isLoading = loading;
-    notifyListeners();
-  }
-
-  bool get error => _hasError;
-  set _error(bool error) {
-    _hasError = error;
-    notifyListeners();
-  }
+  Map<Room, List<DeviceConfig>> get roomsMap => _roomsMap;
+  bool get loading => _loading;
+  bool get hasError => _hasError;
 
   Future<void> _initAsync() async {
     _loading = true;
+    notifyListeners();
     final response = await LightingService.I.readDevices();
 
     switch (response) {
       case Success(value: final devices):
-        _devices = devices;
+        _deviceConfigs = devices;
         final Map<Room, List<DeviceConfig>> groupedRooms = {};
         for (var device in devices) {
           final room = device.room ?? Room.livingRoom;
@@ -50,7 +34,10 @@ class LightingProvider extends ChangeNotifier {
         }
         _roomsMap = groupedRooms;
       case Failure():
-        _error = true;
+        _hasError = true;
     }
+
+    _loading = false;
+    notifyListeners();
   }
 }
