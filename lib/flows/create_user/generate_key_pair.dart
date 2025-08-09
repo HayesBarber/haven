4c1b211fe0c961fa8575c6bd0ed8a8253fd2c49f
Hayes Barber <barberhayes@gmail.com>
2025-08-08 21:27:52 -0400

    auto commit

--- lib/flows/create_user/generate_key_pair.dart
+++ lib/flows/create_user/generate_key_pair.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:haven/flows/create_user/provider/create_user_provider.dart';
-import 'package:provider/provider.dart';
 
 class GenerateKeyPair extends StatelessWidget {
   const GenerateKeyPair({super.key});
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<CreateUserProvider>(context);
-
     return Scaffold(
       body: SizedBox.expand(child: Center(child: Text("Generate Key Pair"))),
     );
