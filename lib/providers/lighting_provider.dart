c692ff7e570b8009160c9ae19c9702cb7144ad79
Hayes Barber <barberhayes@gmail.com>
2025-08-09 18:31:45 -0400

    auto commit

--- lib/providers/lighting_provider.dart
+++ lib/providers/lighting_provider.dart
@@ -1,3 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:haven/services/lighting_service.dart';
+import 'package:haven/utils/result.dart';
+import 'package:home_api_client/home_api_client.dart';
 
-class LightingProvider extends ChangeNotifier {}
+class LightingProvider extends ChangeNotifier {
+  List<DeviceConfig> _deviceConfigs = [];
+  bool _isLoading = false;
+  bool _hasError = false;
+
+  LightingProvider() {
+    _initAsync();
+  }
+
+  List<DeviceConfig> get devices => _deviceConfigs;
+  set _devices(List<DeviceConfig> devices) {
+    _deviceConfigs = devices;
+  }
+
+  bool get loading => _isLoading;
+  set _loading(bool loading) {
+    _isLoading = loading;
+    notifyListeners();
+  }
+
+  bool get error => _hasError;
+  set _error(bool error) {
+    _hasError = error;
+    notifyListeners();
+  }
+
+  Future<void> _initAsync() async {
+    _loading = true;
+    final response = await LightingService.I.readDevices();
+
+    switch (response) {
+      case Success(value: final devices):
+        _devices = devices;
+      case Failure():
+        _error = true;
+    }
+  }
+}
