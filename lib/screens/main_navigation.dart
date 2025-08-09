04fb4281806690679cf789fe45293d7747edd051
Hayes Barber <barberhayes@gmail.com>
2025-08-09 16:44:02 -0400

    page stack

--- lib/screens/main_navigation.dart
+++ lib/screens/main_navigation.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:haven/providers/nav_bar_provider.dart';
+import 'package:provider/provider.dart';
 
 class MainNavigation extends StatelessWidget {
   const MainNavigation({super.key});
   @override
   Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => NavBarProvider())],
+      child: PageStack(),
+    );
+  }
+}
+
+class PageStack extends StatelessWidget {
+  const PageStack({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<NavBarProvider>(context);
+
     return Scaffold(
-      body: SizedBox.expand(child: Center(child: Text('Main Nav'))),
+      body: Stack(
+        children: [
+          IndexedStack(index: provider.selectedIndex, children: provider.pages),
+        ],
+      ),
     );
   }
 }
