ba53f8e7dc2abf98f63bf6ac9f86fd86c7bdbc0d
Hayes Barber <barberhayes@gmail.com>
2025-08-09 12:49:57 -0400

    auto commit

--- lib/services/api_key_service.dart
+++ lib/services/api_key_service.dart
@@ -1,13 +1,33 @@
+import 'dart:convert';
+
 import 'package:gatekeeper_client/gatekeeper_client.dart';
 import 'package:haven/services/local_storage.dart';
+import 'package:haven/utils/logger.dart';
+import 'package:haven/utils/result.dart';
 
 class ApiKeyService {
   ChallengeVerificationResponse? _apiKey;
 
-  Future<void> _initAsync() async {
-    final stored = await LocalStorage.I.read(StorageKey.apiKey);
+  Future<Result<void, Exception>> _initAsync() async {
+    try {
+      final stored = await LocalStorage.I.read(StorageKey.apiKey);
 
-    if (stored == null) return;
+      if (stored == null) {
+        LOGGER.log('No API key stored');
+        return const Success(null);
+      }
+
+      final mapFromStorage = json.decode(stored) as Map<String, dynamic>;
+
+      _apiKey = serializers.deserializeWith(
+        ChallengeVerificationResponse.serializer,
+        mapFromStorage,
+      );
+      LOGGER.log('Loaded API key');
+      return const Success(null);
+    } catch (e) {
+      return Failure(Exception('Failed to load API key: $e'));
+    }
   }
 
   void _getApiKey() async {}
