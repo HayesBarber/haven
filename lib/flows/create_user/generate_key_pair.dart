f8dffb06c9c082363801cf8bdef116885148c227
Hayes Barber <barberhayes@gmail.com>
2025-08-08 22:20:31 -0400

    auto commit

--- lib/flows/create_user/generate_key_pair.dart
+++ lib/flows/create_user/generate_key_pair.dart
@@ -5,7 +5,27 @@ class GenerateKeyPair extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox.expand(child: Center(child: Text("Generate Key Pair"))),
+      body: SizedBox.expand(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: Text(
+                  'Generating Key-Pair...',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ),
+              LinearProgressIndicator(
+                minHeight: 7,
+                borderRadius: BorderRadius.circular(3),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
