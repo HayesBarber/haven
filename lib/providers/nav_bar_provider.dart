0e44734ad575e0fa33e00884a2bfe9fb234e866a
Hayes Barber <barberhayes@gmail.com>
2025-08-11 18:48:15 -0400

    init screen

--- lib/providers/nav_bar_provider.dart
+++ lib/providers/nav_bar_provider.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:haven/screens/lights.dart';
+import 'package:haven/screens/themes.dart';
 
 class NavBarProvider extends ChangeNotifier {
-  final List<Widget> pages = const [Lights()];
+  final List<Widget> pages = const [Lights(), Themes()];
 
   final List<({IconData icon, String title})> icons = const [
     (icon: Icons.lightbulb, title: 'Lights'),
+    (icon: Icons.color_lens, title: 'Themes'),
   ];
 
   int _selectedIndex = 0;
