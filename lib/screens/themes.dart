efc5f3d1397bd1a3af6ad00301228ef0c2160a8f
Hayes Barber <barberhayes@gmail.com>
2025-08-11 19:10:10 -0400

    set loading

--- lib/screens/themes.dart
+++ lib/screens/themes.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:haven/providers/themes_provider.dart';
 import 'package:haven/widgets/scaffold_title.dart';
+import 'package:provider/provider.dart';
 
 class Themes extends StatelessWidget {
   const Themes({super.key});
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ThemesProvider>(context);
+
     return Scaffold(
-      body: ListView(children: [ScaffoldTitle(title: 'Themes')]),
+      body: ListView(
+        children: [ScaffoldTitle(title: 'Themes', loading: provider.loading)],
+      ),
     );
   }
 }
