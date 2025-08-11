920539f42386c39a71ef09a959bc63ae0ef6baf3
Hayes Barber <barberhayes@gmail.com>
2025-08-11 18:40:44 -0400

    read themes

--- lib/services/theme_service.dart
+++ lib/services/theme_service.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:haven/utils/extensions.dart';
 import 'package:haven/utils/http_interceptors.dart';
+import 'package:haven/utils/result.dart';
 import 'package:home_api_client/home_api_client.dart';
 
 class ThemeService {
@@ -10,4 +12,19 @@ class ThemeService {
   ThemeService._();
   static final ThemeService _instance = ThemeService._();
   static ThemeService get I => _instance;
+
+  Future<Result<Map<String, String>, Exception>> readThemes() async {
+    try {
+      final api = _client.getThemesApi();
+
+      final response = await api.getThemesThemesGet();
+      response.assertValid();
+
+      final data = response.data!;
+
+      return Success(data.themes.toMap());
+    } catch (e) {
+      return Failure(Exception('Failed to read themes: $e'));
+    }
+  }
 }
