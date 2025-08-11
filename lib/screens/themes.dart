fa26b6f08d6896830454732e87124ebf8b0923ac
Hayes Barber <barberhayes@gmail.com>
2025-08-11 19:04:06 -0400

    scaffold title

--- lib/screens/themes.dart
+++ lib/screens/themes.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:haven/widgets/scaffold_title.dart';
 
 class Themes extends StatelessWidget {
   const Themes({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox.expand(child: Center(child: const Text('Themes'))),
+      body: ListView(children: [ScaffoldTitle(title: 'Themes')]),
     );
   }
 }
