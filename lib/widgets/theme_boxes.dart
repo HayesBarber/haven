12a05640ed3ae01bdafb226efd7a10da1a1ca3a0
Hayes Barber <barberhayes@gmail.com>
2025-08-11 19:27:28 -0400

    theme boxes

---
+++ lib/widgets/theme_boxes.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ThemeBoxes extends StatelessWidget {
+  final List<Color> colors;
+
+  const ThemeBoxes({super.key, required this.colors});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> boxes = colors
+        .map(
+          (c) => Row(
+            children: [
+              Container(
+                width: 25,
+                height: 12.5,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: c,
+                ),
+              ),
+              const SizedBox(width: 5),
+            ],
+          ),
+        )
+        .toList();
+    return Row(children: boxes);
+  }
+}
