b4fb9901b652ca83d96afd955b13887217073827
Hayes Barber <barberhayes@gmail.com>
2025-08-31 20:48:41 -0400

    auto commit

--- lib/flows/create_theme/create_theme_entry.dart
+++ lib/flows/create_theme/create_theme_entry.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:haven/widgets/exit_button.dart';
 
 class CreateThemeEntry extends StatelessWidget {
   const CreateThemeEntry({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(leading: const ExitButton()),
       body: SizedBox.expand(child: Center(child: Text("Create theme"))),
     );
   }
