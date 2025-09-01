72a6518a940c9e2b4fbf2b9abc65e383fe528210
Hayes Barber <barberhayes@gmail.com>
2025-09-01 19:43:37 -0400

    auto commit

--- lib/widgets/theme_color.dart
+++ lib/widgets/theme_color.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:haven/utils/extensions.dart';
 
 class ThemeColor extends StatelessWidget {
   final Color color;
@@ -14,6 +15,16 @@ class ThemeColor extends StatelessWidget {
           width: 64,
           height: 64,
           decoration: BoxDecoration(color: color, shape: BoxShape.circle),
+        ),
+        Container(
+          width: 25,
+          height: 25,
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            shape: BoxShape.circle,
+            border: Border.all(color: context.colorScheme.surface, width: 2),
+          ),
+          child: Icon(Icons.remove, color: Colors.black),
         ),
       ],
     );
