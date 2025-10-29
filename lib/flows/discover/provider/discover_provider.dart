0b9cadc75d22d31aa4669c9a4a0208a63bb0c530
Hayes Barber <barberhayes@gmail.com>
2025-10-28 21:20:57 -0400

    auto commit

--- lib/flows/discover/provider/discover_provider.dart
+++ lib/flows/discover/provider/discover_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flowkit/flowkit.dart';
 import 'package:forui/forui.dart';
 import 'package:haven/services/discovery_service.dart';
+import 'package:haven/utils/result.dart';
 import 'package:home_api_client/home_api_client.dart';
 
 class DiscoverProvider extends NestedNavigatorProvider {
@@ -10,12 +11,15 @@ class DiscoverProvider extends NestedNavigatorProvider {
   bool _loading = false;
   bool _hasError = false;
   final _service = DiscoveryService();
+  DeviceDiscoveryResponse? _deviceDiscoveryResponse;
 
   DiscoverProvider({required super.navKey});
 
   FSelectTileGroupController<DeviceType> get controller => _controller;
   bool get loading => _loading;
   bool get hasError => _hasError;
+  DeviceDiscoveryResponse? get deviceDiscoveryResponse =>
+      _deviceDiscoveryResponse;
 
   Future<void> discover() async {
     DeviceType? type = _controller.value.firstOrNull;
@@ -28,6 +32,14 @@ class DiscoverProvider extends NestedNavigatorProvider {
     notifyListeners();
 
     final result = await _service.discover(type);
+
+    switch (result) {
+      case Success(value: final value):
+        _hasError = false;
+        _deviceDiscoveryResponse = value;
+      case Failure():
+        _hasError = true;
+    }
 
     _loading = false;
     notifyListeners();
