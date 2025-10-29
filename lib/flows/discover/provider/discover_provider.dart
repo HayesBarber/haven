088992825cc541a199663a24f099ba8969233a71
Hayes Barber <barberhayes@gmail.com>
2025-10-28 21:12:29 -0400

    create getter

--- lib/flows/discover/provider/discover_provider.dart
+++ lib/flows/discover/provider/discover_provider.dart
@@ -3,10 +3,12 @@ import 'package:forui/forui.dart';
 import 'package:home_api_client/home_api_client.dart';
 
 class DiscoverProvider extends NestedNavigatorProvider {
-  final controller = FSelectTileGroupController<DeviceType>.radio(
+  final _controller = FSelectTileGroupController<DeviceType>.radio(
     value: DeviceType.lifx,
   );
   DiscoverProvider({required super.navKey});
+
+  FSelectTileGroupController<DeviceType> get controller => _controller;
 
   Future<void> discover() async {}
 }
