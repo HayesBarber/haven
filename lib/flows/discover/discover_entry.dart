29b2b9548e964a59de4f39f8651c27e755dd935b
Hayes Barber <barberhayes@gmail.com>
2025-10-28 21:09:04 -0400

    auto commit

--- lib/flows/discover/discover_entry.dart
+++ lib/flows/discover/discover_entry.dart
@@ -1,9 +1,42 @@
+import 'package:flowkit/flowkit.dart';
 import 'package:flutter/material.dart';
+import 'package:forui/forui.dart';
+import 'package:haven/flows/discover/provider/discover_provider.dart';
+import 'package:haven/utils/styles.dart';
+import 'package:haven/widgets/scaffold_title.dart';
+import 'package:home_api_client/home_api_client.dart';
+import 'package:provider/provider.dart';
 
 class DiscoverEntry extends StatelessWidget {
   const DiscoverEntry({super.key});
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final provider = Provider.of<DiscoverProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(leading: BackButton(onPressed: () => Navigation.I.pop())),
+      body: ListView(
+        physics: const AlwaysScrollableScrollPhysics(),
+        children: [
+          ScaffoldTitle(title: "Discover Devices"),
+          Padding(
+            padding: Styles.tileGroupPadding,
+            child: FSelectTileGroup(
+              selectController: provider.controller,
+              label: const Text('Device Type'),
+              description: const Text('Select the type of device to discover.'),
+              children: [
+                FSelectTile(title: const Text('Lifx'), value: DeviceType.lifx),
+                FSelectTile(title: const Text('Kasa'), value: DeviceType.kasa),
+                FSelectTile(
+                  title: const Text('ESP'),
+                  value: DeviceType.ledStrip,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/flows/discover/provider/discover_provider.dart
+++ lib/flows/discover/provider/discover_provider.dart
@@ -1,5 +1,10 @@
 import 'package:flowkit/flowkit.dart';
+import 'package:forui/forui.dart';
+import 'package:home_api_client/home_api_client.dart';
 
 class DiscoverProvider extends NestedNavigatorProvider {
+  final controller = FSelectTileGroupController<DeviceType>.radio(
+    value: DeviceType.lifx,
+  );
   DiscoverProvider({required super.navKey});
 }
