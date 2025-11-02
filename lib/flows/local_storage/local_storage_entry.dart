112376a1f0bcc6d7ada86cb56d814f0acbcb32ab
Hayes Barber <barberhayes@gmail.com>
2025-11-02 13:16:45 -0500

    auto commit

--- lib/flows/local_storage/local_storage_entry.dart
+++ lib/flows/local_storage/local_storage_entry.dart
@@ -1,9 +1,21 @@
+import 'package:flowkit/flowkit.dart';
 import 'package:flutter/material.dart';
+import 'package:haven/flows/local_storage/provider/local_storage_provider.dart';
+import 'package:haven/widgets/scaffold_title.dart';
+import 'package:provider/provider.dart';
 
 class LocalStorageEntry extends StatelessWidget {
   const LocalStorageEntry({super.key});
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final provider = Provider.of<LocalStorageProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(leading: BackButton(onPressed: () => Navigation.I.pop())),
+      body: ListView(
+        physics: const AlwaysScrollableScrollPhysics(),
+        children: [ScaffoldTitle(title: 'Local Storage')],
+      ),
+    );
   }
 }
