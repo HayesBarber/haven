11bd330e0a73d8d5058b16fdf2bc601f1439c80e
Hayes Barber <barberhayes@gmail.com>
2025-11-02 13:28:37 -0500

    auto commit

--- lib/flows/local_storage/provider/local_storage_provider.dart
+++ lib/flows/local_storage/provider/local_storage_provider.dart
@@ -1,5 +1,30 @@
 import 'package:flowkit/flowkit.dart';
+import 'package:haven/services/local_storage.dart';
+import 'package:haven/utils/result.dart';
 
 class LocalStorageProvider extends NestedNavigatorProvider {
-  LocalStorageProvider({required super.navKey});
+  LocalStorageProvider({required super.navKey}) {
+    _init();
+  }
+
+  Map<String, String>? _storageData;
+  Map<String, String>? get storageData => _storageData;
+  bool _loading = false;
+  bool get loading => _loading;
+
+  Future<void> _init() async {
+    _loading = true;
+    notifyListeners();
+
+    final result = await LocalStorage.I.readAll();
+    switch (result) {
+      case Success(value: final data):
+        _storageData = data;
+      case Failure():
+        _storageData = null;
+    }
+
+    _loading = false;
+    notifyListeners();
+  }
 }
