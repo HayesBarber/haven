e164d125c676308d64a0e915c20eafe21d2cbe62
Hayes Barber <barberhayes@gmail.com>
2025-11-11 13:08:36 -0500

    auto commit

--- lib/flows/devices/devices_entry.dart
+++ lib/flows/devices/devices_entry.dart
@@ -1,9 +1,22 @@
+import 'package:flowkit/flowkit.dart';
 import 'package:flutter/material.dart';
+import 'package:haven/flows/devices/provider/devices_provider.dart';
+import 'package:haven/widgets/refreshable_scaffold.dart';
+import 'package:provider/provider.dart';
 
 class DevicesEntry extends StatelessWidget {
   const DevicesEntry({super.key});
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final provider = Provider.of<DevicesProvider>(context);
+
+    return RefreshableScaffold(
+      appBar: AppBar(leading: BackButton(onPressed: () => Navigation.I.pop())),
+      title: 'Devices',
+      loading: provider.loading,
+      refreshing: provider.refreshing,
+      onRefresh: provider.refresh,
+      children: [],
+    );
   }
 }
