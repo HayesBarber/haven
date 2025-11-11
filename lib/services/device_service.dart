ceaa1f9e1dbf769b3dbd07e7e05995f86718b9d5
Hayes Barber <barberhayes@gmail.com>
2025-11-11 12:52:30 -0500

    init device service

--- lib/services/device_service.dart
+++ lib/services/device_service.dart
@@ -4,33 +4,28 @@ import 'package:haven/utils/http_interceptors.dart';
 import 'package:haven/utils/result.dart';
 import 'package:home_api_client/home_api_client.dart';
 
-class LightingService {
+class DeviceService {
   final _client = HomeApiClient(
     basePathOverride: dotenv.get('HOME_API_URL'),
     interceptors: HttpInterceptors.getInterceptors(addApiKey: true),
   );
-  LightingService._();
-  static final LightingService _instance = LightingService._();
-  static LightingService get I => _instance;
+  DeviceService._();
+  static final DeviceService _instance = DeviceService._();
+  static DeviceService get I => _instance;
 
-  Future<Result<List<ControllableDevice>, Exception>> controlDevice(
-    String name,
-    PowerAction action,
-  ) async {
+  Future<Result<List<ControllableDevice>, Exception>>
+  readControllableDevices() async {
     try {
-      final api = _client.getLightingApi();
+      final api = _client.getDeviceApi();
 
-      final response = await api.setStateLightingNameActionGet(
-        name: name,
-        action: action,
-      );
+      final response = await api.readAllDevicesDeviceReadGet();
       response.assertValid();
 
       final data = response.data!;
 
       return Success(data.devices.toList());
     } catch (e) {
-      return Failure(Exception('Failed to control device: $e'));
+      return Failure(Exception('Failed to read devices: $e'));
     }
   }
 }
